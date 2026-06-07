8a16d708157b833e2c898842ef2ec523e331ed07
Vladislav_Scvorcov <vladnety134@gmail.com>
2026-06-07 21:57:07 +0300

    test comm

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -16,7 +16,7 @@ import { JwtStrategy } from "./jwt.strategy";
       useFactory: (configService: ConfigService) => ({
         secret: configService.getOrThrow<string>("JWT_SECRET"),
         signOptions: {
-          expiresIn: configService.get<string>("JWT_EXPIRES_IN", "8h") as any,
+          expiresIn: configService.get<string>("JWT_EXPIRES_IN", "24h") as any,
         },
       }),
     }),
